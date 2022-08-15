23c387ea42cc53948db05386a09bd9bb18e49647
gangelo <web.gma@gmail.com>
2022-08-15 13:09:02 -0400

    Initial commit

---
+++ lib/mad_flatter.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mad_flatter/version"
+
+module MadFlatter
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/mad_flatter_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe MadFlatter do
+  it "has a version number" do
+    expect(MadFlatter::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
